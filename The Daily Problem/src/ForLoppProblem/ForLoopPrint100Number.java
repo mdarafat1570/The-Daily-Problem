cc9eb2e3ce7fa1b72c2adc0418c9979563915818
Muhammad Arafat <77748809+mdarafat1570@users.noreply.github.com>
2024-06-08 01:06:40 +0600

    New Push

---
+++ The Daily Problem/src/ForLoppProblem/ForLoopPrint100Number.java
@@ -0,0 +1,9 @@
+package ForLoppProblem;
+
+public class ForLoopPrint100Number {
+    public static void main(String[] args) {
+        for (int i =1; i<=100;i++){
+            System.out.println(i);
+        }
+    }
+}
---
+++ The Daily Problem/src/ForLoppProblem/ForLoopPrint100NumberRevarse.java
@@ -0,0 +1,9 @@
+package ForLoppProblem;
+
+public class ForLoopPrint100NumberRevarse {
+    public static void main(String[] args) {
+        for (int i =100 ;i>=1;i--){
+            System.out.println(i);
+        }
+    }
+}
